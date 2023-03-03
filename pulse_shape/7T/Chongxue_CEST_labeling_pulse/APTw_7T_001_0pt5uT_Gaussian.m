8ab9e13f5570eb24f9f77aeb41eedba909a590d8
ChongxueBie <61711032+ChongxueBie@users.noreply.github.com>
2023-03-03 18:04:07 +0800

    Add files via upload

---
+++ pulse_shape/7T/Chongxue_CEST_labeling_pulse/APTw_7T_001_0pt5uT_Gaussian.m
@@ -0,0 +1,131 @@
+%% APTw_3T_001_2uT_36SincGauss_DC90_2s_braintumor
+% Creates a sequence file for an APTw protocol with Sinc-Gaussian pulses, 90% DC and tsat of 2 s
+%
+% Kai Herz 2020
+% kai.herz@tuebingen.mpg.de
+
+clear all
+close all
+clc
+
+% author name for sequence file
+author = 'Kai Herz';
+
+%% get id of generation file
+if contains(mfilename, 'LiveEditorEvaluationHelperESectionEval')
+    [~, seqid] = fileparts(matlab.desktop.editor.getActiveFilename);
+else
+    [~, seqid] = fileparts(which(mfilename));
+end
+
+%% sequence definitions
+% everything in seq_defs gets written as definition in .seq-file
+seq_defs.tp            = 0.08           ; % pulse duration [s]
+seq_defs.td            = 0.005            ; % interpulse delay [s]
+seq_defs.Trec          = 5             ; % recovery time [s]
+seq_defs.Trec_M0       = 5             ; % recovery time before M0 [s]
+seq_defs.M0_offset     = -1560           ; % m0 offset [ppm]
+seq_defs.DCsat         = (seq_defs.tp)/(seq_defs.tp+seq_defs.td); % duty cycle
+seq_defs.offsets_ppm   = [seq_defs.M0_offset -6:0.1:6]; % offset vector [ppm]
+seq_defs.num_meas      = numel(seq_defs.offsets_ppm)   ; % number of repetition
+% seq_defs.Tsat          = seq_defs.n_pulses*(seq_defs.tp+seq_defs.td) - ...
+%                          seq_defs.td ;  % saturation time [s]
+seq_defs.Tsat          = 4 ;  % saturation time [s]
+seq_defs.n_pulses      = round(4/(seq_defs.tp + seq_defs.td))              ; % number of pulses 38*(0.1+0.005)
+
+seq_defs.B0            = 7               ; % B0 [T]
+seq_defs.seq_id_string = seqid           ; % unique seq id
+
+
+%% get info from struct
+offsets_ppm = seq_defs.offsets_ppm; % [ppm]
+Trec        = seq_defs.Trec;        % recovery time between scans [s]
+Trec_M0     = seq_defs.Trec_M0;     % recovery time before m0 scan [s]
+tp          = seq_defs.tp;          % sat pulse duration [s]
+td          = seq_defs.td;          % delay between pulses [s]
+n_pulses    = seq_defs.n_pulses;    % number of sat pulses per measurement. if DC changes use: n_pulses = round(2/(t_p+t_d))
+B0          = seq_defs.B0;          % B0 [T]
+B1pa        = 0.4175;  % mean sat pulse b1 [uT]
+spoiling    = 0;     % 0=no spoiling, 1=before readout, Gradient in x,y,z
+
+seq_filename = strcat(seq_defs.seq_id_string,'.seq'); % filename
+
+%% scanner limits
+% see pulseq doc for more ino
+seq = SequenceSBB(getScannerLimits());
+
+%% create scanner events
+% satpulse
+gyroRatio_hz  = 42.5764;                  % for H [Hz/uT]
+gyroRatio_rad = gyroRatio_hz*2*pi;        % [rad/uT]
+fa_sat        = B1pa*gyroRatio_rad*tp % flip angle of sat pulse
+fa_sat/(2*pi)*360
+% create pulseq saturation pulse object
+
+%satPulse      = mr.makeGaussPulse(fa_sat, 'Duration', t_p,'system',lims,'timeBwProduct', 0.2,'apodization', 0.5); % siemens-like gauss
+% satPulse      = mr.makeSincPulse(fa_sat, 'Duration', tp, 'system', seq.sys,'timeBwProduct', 2,'apodization', 0.15); % philips-like sinc
+% satPulse      = mr.makeBlockPulse(fa_sat, 'Duration', tp, 'system', seq.sys);
+satPulse      = mr.makeGaussPulse(fa_sat, 'Duration', tp,'system',seq.sys,'timeBwProduct', 0.2,'apodization', 0.5); % siemens-like gauss
+
+[B1cwpe,B1cwae,B1cwae_pure,alpha]= calculatePowerEquivalents(satPulse,tp,td,1,gyroRatio_hz);
+seq_defs.B1cwpe = B1cwpe;
+B1cwpe
+
+%% loop through zspec offsets
+offsets_Hz = offsets_ppm*gyroRatio_hz*B0;
+
+% loop through offsets and set pulses and delays
+for currentOffset = offsets_Hz
+    if currentOffset == seq_defs.M0_offset*gyroRatio_hz*B0
+        if Trec_M0 > 0
+            seq.addBlock(mr.makeDelay(Trec_M0));
+        end
+    else
+        if Trec > 0
+            seq.addBlock(mr.makeDelay(Trec)); % recovery time
+        end
+    end
+    satPulse.freqOffset = currentOffset; % set freuqncy offset of the pulse
+    accumPhase=0;
+    for np = 1:n_pulses
+        satPulse.phaseOffset = 0; %mod(accumPhase,2*pi); % set accumulated pahse from previous rf pulse
+%         satPulse.phaseOffset = mod(accumPhase,2*pi); % set accumulated pahse from previous rf pulse
+        
+        seq.addBlock(satPulse) % add sat pulse
+        % calc phase for next rf pulse
+        accumPhase = mod(accumPhase + currentOffset*2*pi*(numel(find(abs(satPulse.signal)>0))*1e-6),2*pi);
+        if np < n_pulses % delay between pulses
+            seq.addBlock(mr.makeDelay(td)); % add delay
+        end
+    end
+    if spoiling % spoiling before readout
+        seq.addSpoilerGradients()
+    end
+    seq.addPseudoADCBlock(); % readout trigger event
+end
+
+
+
+%% write definitions
+def_fields = fieldnames(seq_defs);
+for n_id = 1:numel(def_fields)
+    seq.setDefinition(def_fields{n_id}, seq_defs.(def_fields{n_id}));
+end
+seq.write(seq_filename, author);
+
+%% plot
+saveSaturationPhasePlot(seq_filename);
+
+%% call standard sim
+% M_z = simulate_pulseqcest(seq_filename,'../../sim-library/cest_labeling_pulse_3T_bmsim.yaml');
+M_z = simulate_pulseqcest(seq_filename,'../../sim-library/cest_labeling_pulse_7T_bmsim.yaml');
+
+%% plot
+plotSimulationResults(M_z,offsets_ppm, seq_defs.M0_offset);
+
+%% save
+saveDir = '/Volumes/T7/CEST_labeling/data/pulse_shape/7T/';
+% saveDir = '/Volumes/T7/CEST_labeling/data/pulse_shape/3T/';
+
+filename=[saveDir,'Z-spectrum_Gaussian_no_Phase.mat'];
+save(filename,'M_z');
